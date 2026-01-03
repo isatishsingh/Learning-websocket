e7975e865332a54a28b9481bef85cacae6a537b2
isatishrajput <chauhansatish978@gmail.com>
2026-01-03 17:58:24 +0530

    Making proper format or project

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer } from "ws";
-
-const ws = new WebSocketServer({port:8080});
-
-ws.on("connection",function(socket){
-    console.log("Hello Connection happened");
-    socket.send("Hello, we're from server side, we came here to help you");
-});
